b2671542ac5d8a56d5e03a7546f0e64051ed69e1
Joel Gallant <joel@joelgallant.me>
2019-03-14 21:04:46 -0600

    Adds benchmark for structuredMapper

---
+++ src/bench.js
@@ -0,0 +1,24 @@
+const { Suite } = require('benchmark');
+const { structuredMapper } = require('..');
+
+const suite = new Suite();
+
+const array = Array(100000).fill(undefined).map(_ => Math.random());
+
+// this is within the margin of error
+suite
+  .add('array mapping', () => {
+    const res = array.map(f => f.toString());
+  })
+  .add('structured mapping', () => {
+    const res = structuredMapper(array, { array: true, map: val => val.toString() });
+  });
+
+suite
+  .on('cycle', function(event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function() {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  .run();
